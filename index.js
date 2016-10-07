68aff2f41ecbc8d93909cd838944e842fccbafc1
Morgan 'ARR\!' Allen <morganrallen@gmail.com>
2016-10-07 11:20:08 -0700

    Threshold was average 1 point off. #derp

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var DEFAULT_COLOR = "rgba(0, 255, 0, 0.3)";
 
 const STEP = 1 / 100;
 const MIN_AVG = 100;
-const MAX_AVG = 134;
+const MAX_AVG = 140;
 const AVG_DEVIATION = 70;
 const COLOR_THRESHOLD = 55;
 
